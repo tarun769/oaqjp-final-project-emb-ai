755a0f6012279c571cc0901d8503bd3d7050e619
tarun769 <tarun699.com>
2025-07-17 21:19:41 -0400

    new commit

--- final_project/EmotionDetection/emotion_detection.py
+++ final_project/EmotionDetection/emotion_detection.py
@@ -1,27 +1,35 @@
-import requests  # Import the requests library to handle HTTP requests
+import requests
 import json
 
-def emotion_detector(text_to_analyze):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+def emotion_detector(text_to_analyze):
+    """
+    Detects emotions in the given text using a Watson NLP API.
+
+    Args:
+        text_to_analyze (str): The text content to be analyzed for emotions.
+
+    Returns:
+        dict: A dictionary containing the scores for 'anger', 'disgust',
+              'fear', 'joy', 'sadness', and the 'dominant_emotion'.
+              Returns None for all emotions if the API call fails.
+    """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     
     data = json.loads(response.text)
-    emotions_data = data['emotionPredictions'][0]['emotion']
-    anger_score = emotions_data['anger']
-    disgust_score = emotions_data['disgust']
-    fear_score = emotions_data['fear']
-    joy_score = emotions_data['joy']
-    sadness_score = emotions_data['sadness']
-    dominant_emotion = max(emotions_data, key=emotions_data.get)
+    emotions_list = ['anger', 'disgust', 'fear', 'joy', 'sadness', 'dominant_emotion']
+    emotion_dict = {}
 
-    # return anger_score
-    return {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score,
-        'dominant_emotion': dominant_emotion
-    }
+    for key in emotions_list:
+        if response.status_code == 400:
+            emotion_dict[key] = None
+        else:
+            emotions_data = data['emotionPredictions'][0]['emotion']
+            if key == 'dominant_emotion':
+                emotion_dict['dominant_emotion'] = max(emotions_data, key=emotions_data.get)
+            else:
+                emotion_dict[key] = emotions_data[key]
+
+    return emotion_dict
